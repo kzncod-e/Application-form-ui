c54eda06b72005e942327778c1f5f8ba07250ec7
kzncod-e <kznrehan@gmail.com>
2024-11-22 17:03:21 +0700

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Application-form-ui/", // Set to the `homepage` path in package.json
+   // Set to the `homepage` path in package.json
 });
